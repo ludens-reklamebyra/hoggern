52c8fbe51d1a48564d263ac4a13dcc15d19cc46c
Stian Bakkane <stianba@gmail.com>
2015-11-30 13:04:45 +0100

    Soft facking tabs

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,8 +3,8 @@ import ReactDOMServer from 'react-dom/server';
 import Game from '../components/Game';
 
 export default function(app) {
-	app.get('/', (req, res) => {
-		const markup = ReactDOMServer.renderToString(<Game />);
-		res.render('game', {game: markup});
-	});
+  app.get('/', (req, res) => {
+    const markup = ReactDOMServer.renderToString(<Game />);
+    res.render('game', {game: markup});
+  });
 }
